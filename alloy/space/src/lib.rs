b53516ed1e6ea86187beabf24d5a0a7c56513085
Rohan <Rohan@MacBook-Pro.local>
2023-06-16 10:07:34 -0700

    updated crate-level documentation

--- alloy/space/src/lib.rs
+++ alloy/space/src/lib.rs
@@ -6,10 +6,6 @@
 //!
 //! Points of arbitrary dimension are also represented; support is
 //! given particularly for `Point2` and `Point3`.
-//!
-//! At the core of all of the foregoing structs are two types: `Real`
-//! and `Complex`, which encode all the structure of their eponymous
-//! algebraic fields.
 
 #![warn(missing_docs)]
 #![deny(rust_2018_idioms)]
