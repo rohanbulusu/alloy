5b014d8c63d2f60f617c401221260b26c0b0004f
Rohan <Rohan@MacBook-Pro.local>
2023-06-18 16:28:31 -0700

    made Point2::left_of and Point2::right_of inline

--- alloy/space/src/points.rs
+++ alloy/space/src/points.rs
@@ -367,6 +367,7 @@ impl Point2 {
 	/// assert!(a.left_of(&b));
 	/// assert!(!b.left_of(&a));
 	/// ```
+	#[inline]
 	pub fn left_of(&self, other: &Self) -> bool {
 		self.x < other.x
 	}
@@ -381,6 +382,7 @@ impl Point2 {
 	/// assert!(a.right_of(&b));
 	/// assert!(!b.right_of(&a));
 	/// ```
+	#[inline]
 	pub fn right_of(&self, other: &Self) -> bool {
 		self.x > other.x
 	}
